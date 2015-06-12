ada05c85dfa802268b7b2f5b66567d086f85f1ce
OTER <maxymhammer@yandex.ru>
2015-06-12 13:09:06 +0300

    Project recreation. Fixed cell coordinates

--- src/Snake/CeilGenerator.java
+++ src/Snake/CeilGenerator.java
@@ -1,23 +1,15 @@
 package Snake;
 
-/**
- * Created by NEKTO on 18.04.2015.
- */
 public class CeilGenerator {
     private int hexPointsX[];
     private int hexPointsY[];
 
-    /**
-     * It's create points of Hex
-     * @param n number of edge figure
-     * @param ceilRadius radius of Hex
-     */
     public CeilGenerator(int n, int ceilRadius) {
         hexPointsX = new int[n];
         hexPointsY = new int[n];
         for (int i = 0; i < n; i++) {
-            hexPointsX[i] = (int) (ceilRadius * Math.sin(((double)i / n) * 2 * Math.PI) + ceilRadius);
-            hexPointsY[i] = (int) (ceilRadius * Math.cos(((double)i / n) * 2 * Math.PI) + ceilRadius);
+            hexPointsX[i] = (int)Math.round(ceilRadius * Math.sin(((double)i / n) * 2 * Math.PI) + ceilRadius);
+            hexPointsY[i] = (int) Math.round(ceilRadius * Math.cos(((double) i / n) * 2 * Math.PI) + ceilRadius);
         }
     }
 
